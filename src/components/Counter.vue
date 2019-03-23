ac4cd264d89cedecfeb0ac7aedbd28580301d13f
Administrator <814579918@qq.com>
2019-03-24 00:04:12 +0800

    父子组件之间的通信

---
+++ src/components/Counter.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <button @click="increment">+</button>
+    <button v-on:click="decrement">-</button>
+    <p><span>{{num}}</span></p>
+  </div>
+
+</template>
+
+<script>
+    export default {
+        name: "Counter",
+      props:["num"],
+      data(){
+            return{
+
+            }
+      },
+      methods:{
+        increment(){
+          this.$emit("incre");
+        },
+        decrement(){
+          this.$emit("decre");
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
